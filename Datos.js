0b928eabe011809f6f6ac33e15ca06dbd6d59747
Cappybarahook <vasquezguillermo124@gmail.com>
2023-09-12 22:17:52 -0300

    Mir primer commit java

---
+++ Datos.js
@@ -0,0 +1,15 @@
+let edad = 20;
+let sueldo = 200000;
+const PI = 2.14;
+
+const n_grande = 1e6; // 1 millon
+const n_pequenho = 1e-6; // 0.000001
+
+const bigInt = 123456789012345678901234567890;
+
+let string1 = "Hola mundo";
+let string2 = 'Buenas tardes';
+let frase = `Este es un saludo: ${string1}`;
+
+alert('Hola mundo');
+console.log('goa')
